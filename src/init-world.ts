928dd3fc6a0c421fc8530a4dd640de447bf6e546
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-04 19:06:46 -0700

    add initial entity

--- src/init-world.ts
+++ src/init-world.ts
@@ -1,9 +1,27 @@
 import { World } from './schema'
+import { Vec2 } from './vec2'
 
 export async function initWorld(): Promise<World> {
+  const entities: World['entities'] = {}
+  let nextEntityId = 0
+
+  function addEntity(
+    position: { x: number; y: number },
+    color: string,
+  ) {
+    const id = `${nextEntityId++}`
+    entities[id] = {
+      id,
+      position,
+      color,
+    }
+  }
+
+  addEntity(new Vec2(0, 0), 'red')
+
   return {
     tick: 0,
-    entities: {},
-    nextEntityId: 0,
+    entities,
+    nextEntityId,
   }
 }
