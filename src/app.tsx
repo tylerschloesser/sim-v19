4a4ea5ad937533dc6f6785fb9c9f35e26edb5751
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-05 22:07:34 -0700

    update buttons

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,57 @@
+import { useStateObservable } from '@react-rxjs/core'
+import clsx from 'clsx'
+import { useContext } from 'react'
 import { ActionButton } from './action-button'
+import { AppContext } from './app-context'
 import { CursorComponent } from './cursor-component'
 
 export function App() {
   return (
     <>
       <div className="absolute bottom-0 w-full flex justify-center">
-        <div className="p-8">
+        <div
+          className={clsx('flex items-center gap-8 p-8')}
+        >
+          <RobotButton />
           <ActionButton />
+          <TbdButton />
         </div>
       </div>
       <CursorComponent />
     </>
+  )
+}
+
+function RobotButton() {
+  const context = useContext(AppContext)
+  const attachedRobotId = useStateObservable(
+    context.attachedRobotId$,
+  )
+  const label = attachedRobotId ? 'Detach' : ''
+  return (
+    <div
+      className={clsx(
+        'w-16 h-16 rounded-full bg-white',
+        'flex justify-center items-center',
+        'text-xs',
+        !attachedRobotId && 'opacity-50',
+      )}
+    >
+      {label}
+    </div>
+  )
+}
+
+function TbdButton() {
+  return (
+    <div
+      className={clsx(
+        'w-16 h-16 rounded-full bg-white',
+        'flex justify-center items-center',
+        'text-xs',
+      )}
+    >
+      Tbd
+    </div>
   )
 }
