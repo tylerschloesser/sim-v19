1e167d7fc97a868f84a045221afffde4d3c290bb
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-06 15:32:19 -0700

    fix world

--- src/world-component.tsx
+++ src/world-component.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import { combineLatest, takeUntil } from 'rxjs'
+import invariant from 'tiny-invariant'
+import { AppContext } from './app-context'
+import { useEffectWithDestroy } from './use-effect-with-destroy'
 
 export type WorldComponentProps =
   React.PropsWithChildren<{}>
@@ -6,5 +10,29 @@ export type WorldComponentProps =
 export function WorldComponent({
   children,
 }: WorldComponentProps) {
-  return <>{children}</>
+  const ref = useRef<HTMLDivElement>(null)
+  const { camera$, viewport$, scale$ } =
+    useContext(AppContext)
+
+  useEffectWithDestroy(
+    (destroy$) => {
+      combineLatest([camera$, viewport$, scale$])
+        .pipe(takeUntil(destroy$))
+        .subscribe(([camera, viewport, scale]) => {
+          invariant(ref.current)
+          const transform = camera
+            .mul(scale)
+            .sub(viewport.div(2))
+            .mul(-1)
+          ref.current.style.transform = `translate(${transform.x}px, ${transform.y}px)`
+        })
+    },
+    [camera$, viewport$, scale$],
+  )
+
+  return (
+    <div className="absolute" ref={ref}>
+      {children}
+    </div>
+  )
 }
