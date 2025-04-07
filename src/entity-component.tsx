38db8cf66cc9b8166a354170e63fe9863a4e103d
Tyler Schloesser <tylerschloesser@gmail.com>
2025-04-06 17:00:43 -0700

    render resource as grid

--- src/entity-component.tsx
+++ src/entity-component.tsx
@@ -31,14 +31,6 @@ export const EntityComponent = React.memo(
               scale,
             )
             ref.current.style.transform = `translate(${x}px, ${y}px)`
-
-            switch (entity.type) {
-              case entityTypeSchema.enum.Resource: {
-                ref.current.style.backgroundColor =
-                  entity.color
-                break
-              }
-            }
           })
       },
       [scale$, entity],
@@ -73,6 +65,45 @@ const ResourceEntityComponent = React.memo(
   function ResourceEntityComponent({
     color,
   }: ResourceEntityComponentProps) {
-    return <>{color}!</>
+    const { scale$ } = useContext(AppContext)
+    const scale = useStateObservable(scale$)
+
+    const rects: { x: number; y: number }[] = []
+
+    const GRID_SIZE = 6
+    for (let x = 0; x < GRID_SIZE; x++) {
+      for (let y = 0; y < GRID_SIZE; y++) {
+        switch (x % 2) {
+          case 0:
+            if (y % 2 === 0) {
+              rects.push({ x, y })
+            }
+            break
+          case 1:
+            if (y % 2 === 1) {
+              rects.push({ x, y })
+            }
+            break
+        }
+      }
+    }
+
+    return (
+      <svg
+        viewBox={`0 0 ${scale} ${scale}`}
+        className="w-full h-full"
+      >
+        {rects.map(({ x, y }, i) => (
+          <rect
+            key={i}
+            x={x * (scale / GRID_SIZE)}
+            y={y * (scale / GRID_SIZE)}
+            width={scale / GRID_SIZE}
+            height={scale / GRID_SIZE}
+            fill={color}
+          />
+        ))}
+      </svg>
+    )
   },
 )
